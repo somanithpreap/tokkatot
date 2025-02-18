bca64df15ff986abffd33bca07f4752aaf0b9d69
Heng Osborn <158818815+SirOsborn@users.noreply.github.com>
2025-02-18 23:09:32 +0700

    Delete js directory

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-// js/navbar.js
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("../components/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML("beforeend", data);
-        })
-        .catch(error => console.error("Error loading navbar:", error));
-});
