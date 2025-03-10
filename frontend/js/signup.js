769d1b345e4352cab0c2d1c76d60e71d1e3a5ee8
SirOsborn <sunhenglong168@gmail.com>
2025-03-11 02:42:05 +0700

    Add error handling for registration form and display error messages in Khmer

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -2,150 +2,165 @@ document.querySelector("form").action = getURL() + "/register";
 document.getElementById("login-link").href = getURL() + "/login";
 
 const form = document.querySelector("form"),
-	uField = form.querySelector(".username"),
-	uInput = uField.querySelector("input"),
-	pField = form.querySelector(".password"),
-	pInput = pField.querySelector("input"),
-	cpField = form.querySelector(".confirm-password"),
-	cpInput = cpField.querySelector("input"),
-	kField = form.querySelector(".registration-key"),
-	kInput = kField.querySelector("input");
+  uField = form.querySelector(".username"),
+  uInput = uField.querySelector("input"),
+  pField = form.querySelector(".password"),
+  pInput = pField.querySelector("input"),
+  cpField = form.querySelector(".confirm-password"),
+  cpInput = cpField.querySelector("input"),
+  kField = form.querySelector(".registration-key"),
+  kInput = kField.querySelector("input");
 
 // Get the toggle password icons
 const togglePassword = document.querySelector(".toggle-password");
 const toggleConfirmPassword = document.querySelector(
-	".toggle-confirm-password",
+  ".toggle-confirm-password",
 );
 
 // Password toggle functionality
 document.addEventListener("DOMContentLoaded", () => {
-	// Toggle password visibility
-	togglePassword.addEventListener("click", function () {
-		const type =
-			pInput.getAttribute("type") === "password" ? "text" : "password";
-		pInput.setAttribute("type", type);
-		this.classList.toggle("fa-eye");
-		this.classList.toggle("fa-eye-slash");
-	});
+  // Toggle password visibility
+  togglePassword.addEventListener("click", function () {
+    const type =
+      pInput.getAttribute("type") === "password" ? "text" : "password";
+    pInput.setAttribute("type", type);
+    this.classList.toggle("fa-eye");
+    this.classList.toggle("fa-eye-slash");
+  });
 
-	// Toggle confirm password visibility
-	toggleConfirmPassword.addEventListener("click", function () {
-		const type =
-			cpInput.getAttribute("type") === "password" ? "text" : "password";
-		cpInput.setAttribute("type", type);
-		this.classList.toggle("fa-eye");
-		this.classList.toggle("fa-eye-slash");
-	});
+  // Toggle confirm password visibility
+  toggleConfirmPassword.addEventListener("click", function () {
+    const type =
+      cpInput.getAttribute("type") === "password" ? "text" : "password";
+    cpInput.setAttribute("type", type);
+    this.classList.toggle("fa-eye");
+    this.classList.toggle("fa-eye-slash");
+  });
 });
 
 form.addEventListener("submit", function (event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	checkUsername();
-	checkPass();
-	checkConfirmPass();
-	checkRegKey();
+  checkUsername();
+  checkPass();
+  checkConfirmPass();
+  checkRegKey();
 
-	if (
-		!uField.classList.contains("error") &&
-		!pField.classList.contains("error") &&
-		!cpField.classList.contains("error") &&
-		!kField.classList.contains("error")
-	) {
-		const formData = new FormData(form);
+  if (
+    !uField.classList.contains("error") &&
+    !pField.classList.contains("error") &&
+    !cpField.classList.contains("error") &&
+    !kField.classList.contains("error")
+  ) {
+    const formData = new FormData(form);
 
-		fetch(form.action, {
-			method: "POST",
-			body: formData,
-		}).then((response) => {
-			if (response.ok) window.location.href = "/";
-			else {
-				// handleServerErrors(error);
-				return response.json().then((error) => {
-					console.error("Error: ", error.error);
-				});
-			}
-		});
-	}
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+    }).then((response) => {
+      if (response.ok) window.location.href = "/";
+      else {
+        return response.json().then((error) => {
+          handleServerErrors(error.error);
+          console.error("Error: ", error.error);
+        });
+      }
+    });
+  }
 });
 
 function handleServerErrors(error) {
-	// Use switch-case to handle the errors
-	if (error.username) {
-		uField.classList.add("error");
-		let errorTxt = uField.querySelector(".error-txt");
-		errorTxt.innerText = error.username;
-	}
-	if (error.password) {
-		pField.classList.add("error");
-		let errorTxt = pField.querySelector(".error-txt");
-		errorTxt.innerText = error.password;
-	}
-	if (error.confirmPassword) {
-		cpField.classList.add("error");
-		let errorTxt = cpField.querySelector(".error-txt");
-		errorTxt.innerText = error.confirmPassword;
-	}
-	if (error.registrationKey) {
-		kField.classList.add("error");
-		let errorTxt = kField.querySelector(".error-txt");
-		errorTxt.innerText = error.registrationKey;
-	}
+  if (error.username) {
+    uField.classList.add("error");
+    let errorTxt = uField.querySelector(".error-txt");
+    errorTxt.innerText = error.username;
+  } else if (error == "Username already exists") {
+    uField.classList.add("error");
+    let errorTxt = uField.querySelector(".error-txt");
+    errorTxt.innerText = "ឈ្មោះអ្នកប្រើប្រាស់មានរួចហើយ";
+  }
+  if (error.password) {
+    pField.classList.add("error");
+    let errorTxt = pField.querySelector(".error-txt");
+    errorTxt.innerText = error.password;
+  } else if (error == "Password too weak") {
+    pField.classList.add("error");
+    let errorTxt = pField.querySelector(".error-txt");
+    errorTxt.innerText = "ពាក្យសម្ងាត់ខ្សោយពេក";
+  }
+  if (error.confirmPassword) {
+    cpField.classList.add("error");
+    let errorTxt = cpField.querySelector(".error-txt");
+    errorTxt.innerText = error.confirmPassword;
+  } else if (error == "Passwords do not match") {
+    cpField.classList.add("error");
+    let errorTxt = cpField.querySelector(".error-txt");
+    errorTxt.innerText = "ពាក្យសម្ងាត់មិនត្រូវគ្នា";
+  }
+  if (error.registrationKey) {
+    kField.classList.add("error");
+    let errorTxt = kField.querySelector(".error-txt");
+    errorTxt.innerText = error.registrationKey;
+  } else if (error == "Invalid registration key") {
+    kField.classList.add("error");
+    let errorTxt = kField.querySelector(".error-txt");
+    errorTxt.innerText = "លេខកូដចុះឈ្មោះមិនត្រឹមត្រូវ";
+  }
 }
 
 function checkUsername() {
-	if (uInput.value == "") {
-		uField.classList.add("error");
-		uField.classList.remove("valid");
-		let errorTxt = uField.querySelector(".error-txt");
-		errorTxt.innerText = "Username can't be blank";
-	} else {
-		uField.classList.remove("error");
-		uField.classList.add("valid");
-	}
+  if (uInput.value == "") {
+    uField.classList.add("error");
+    uField.classList.remove("valid");
+    let errorTxt = uField.querySelector(".error-txt");
+    errorTxt.innerText = "Username can't be blank";
+  } else {
+    uField.classList.remove("error");
+    uField.classList.add("valid");
+  }
 }
 
 function checkPass() {
-	if (pInput.value == "") {
-		pField.classList.add("error");
-		pField.classList.remove("valid");
-		let errorTxt = pField.querySelector(".error-txt");
-		errorTxt.innerText = "Password can't be blank";
-	} else if (pInput.value.length < 8) {
-		pField.classList.add("error");
-		pField.classList.remove("valid");
-		let errorTxt = pField.querySelector(".error-txt");
-		errorTxt.innerText = "Password must be at least 8 characters";
-	} else {
-		pField.classList.remove("error");
-		pField.classList.add("valid");
-	}
+  if (pInput.value == "") {
+    pField.classList.add("error");
+    pField.classList.remove("valid");
+    let errorTxt = pField.querySelector(".error-txt");
+    errorTxt.innerText = "Password can't be blank";
+  } else if (pInput.value.length < 8) {
+    pField.classList.add("error");
+    pField.classList.remove("valid");
+    let errorTxt = pField.querySelector(".error-txt");
+    errorTxt.innerText = "Password must be at least 8 characters";
+  } else {
+    pField.classList.remove("error");
+    pField.classList.add("valid");
+  }
 }
 
 function checkConfirmPass() {
-	if (cpInput.value == "" || cpInput.value !== pInput.value) {
-		cpField.classList.add("error");
-		cpField.classList.remove("valid");
-		let errorTxt = cpField.querySelector(".error-txt");
-		cpInput.value != ""
-			? (errorTxt.innerText = "Passwords do not match")
-			: (errorTxt.innerText = "Confirm Password can't be blank");
-	} else {
-		cpField.classList.remove("error");
-		cpField.classList.add("valid");
-	}
+  if (cpInput.value == "" || cpInput.value !== pInput.value) {
+    cpField.classList.add("error");
+    cpField.classList.remove("valid");
+    let errorTxt = cpField.querySelector(".error-txt");
+    cpInput.value != ""
+      ? (errorTxt.innerText = "Passwords do not match")
+      : (errorTxt.innerText = "Confirm Password can't be blank");
+  } else {
+    cpField.classList.remove("error");
+    cpField.classList.add("valid");
+  }
 }
 
 function checkRegKey() {
-	if (kInput.value == "") {
-		kField.classList.add("error");
-		kField.classList.remove("valid");
-		let errorTxt = kField.querySelector(".error-txt");
-		errorTxt.innerText = "Registration key can't be blank";
-	} else {
-		kField.classList.remove("error");
-		kField.classList.add("valid");
-	}
+  if (kInput.value == "") {
+    kField.classList.add("error");
+    kField.classList.remove("valid");
+    let errorTxt = kField.querySelector(".error-txt");
+    errorTxt.innerText = "Registration key can't be blank";
+  } else {
+    kField.classList.remove("error");
+    kField.classList.add("valid");
+  }
 }
 
 /*
