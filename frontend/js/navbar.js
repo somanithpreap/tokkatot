34c1356dd9c5e1ba8d777058e64ce2cb8e07e7dc
SirOsborn <sunhenglong168@gmail.com>
2025-03-13 15:36:34 +0700

    commented out dashboard page

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,13 +1,13 @@
 // js/navbar.js
 document.addEventListener("DOMContentLoaded", function () {
-	fetch("../components/navbar.html")
-		.then((response) => response.text())
-		.then((data) => {
-			document.body.insertAdjacentHTML("beforeend", data);
+  fetch("../components/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("beforeend", data);
 
-			document.getElementById("nav-home").href = getURL();
-			document.getElementById("nav-dashboard").href = getURL() + "/dashboard";
-			document.getElementById("nav-settings").href = getURL() + "/settings";
-		})
-		.catch((error) => console.error("Error loading navbar:", error));
+      document.getElementById("nav-home").href = getURL();
+      //document.getElementById("nav-dashboard").href = getURL() + "/dashboard";
+      document.getElementById("nav-settings").href = getURL() + "/settings";
+    })
+    .catch((error) => console.error("Error loading navbar:", error));
 });
